594f848bec9d9e6584a5b99953d2bfb6ada3d833
Vinay Kushwaha <Vinay_Kushwaha@hotmail.com>
2021-05-23 21:53:47 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage =() =>(
-  <div>
-    HATS PAGE
-  </div>
-)
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-      <Switch >
-        <Route exact={true} path='/' component={HomePage}/>
-        <Route exact={true} path='/hats' component={HatsPage}/>
-
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
-   
-     
-
     </div>
   );
 }
